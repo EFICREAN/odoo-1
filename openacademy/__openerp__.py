2b98dd8cca544b0b10d68be0b67dfbbeeb2ce8a2
Paul <porellana.mwb@gmail.com>
2015-05-07 09:48:11 -0400

    ok070515

--- openacademy/__openerp__.py
+++ openacademy/__openerp__.py
@@ -21,7 +21,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'templates.xml',
+        'templates.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
--- openacademy/models.py
+++ openacademy/models.py
@@ -6,7 +6,7 @@ class openacademy_course(models.Model):
 	_name = 'openacademy.course'
 	name = fields.Char(string="Title", required=True)
 	description = fields.Text(string="Description")
-	responsible_id = fields.Many2one('res.users', string="Responsible")
+	responsible_id = fields.Many2one('res.users',ondelete='set null', string="Responsible", index = True)
 	session_ids = fields.One2many('openacademy.session','course_id',string="session")
 class openacademy_session(models.Model):
 	_name = 'openacademy.session'
@@ -14,10 +14,10 @@ class openacademy_session(models.Model):
 	start_date = fields.Date(string='Start Date')
 	duration = fields.Float(string='Duration',digits=(6,2),help="Duration in Hours")
 	seats = fields.Integer(string="Seat Number")
-	course_id=fields.Many2one('openacademy.course',string="Course")
 	instructor_id = fields.Many2one('res.partner',string="Instructor")
+	course_id=fields.Many2one('openacademy.course',ondelete='cascade',string="Course",required=True)
 	attendee_ids = fields.One2many('openacademy.attendee','session_id',string="Attendees")
-   
+        attendee_ids2= fields.Many2many('res.partner',string="Attendees2") 
 
 class openacademy_attendee(models.Model):
     _name = 'openacademy.attendee'
