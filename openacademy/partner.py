2717793c216578461da3e43bca965d0607d7cc16
root <root@odoo.(none)>
2015-08-29 23:29:13 -0400

    ok

--- openacademy/partner.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from openerp import models, fields, api
-
-class Partner(models.Model):
-    _inherit = 'res.partner'
-    instructor = fields.Boolean("Instructor", default=False)
-    session_ids = fields.Many2many('openacademy.session', string="Attended Sessions", readonly=True)
-
