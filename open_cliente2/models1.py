8a70f8a7bfe9ef3c6c90040a36905e4ed1b158c2
root <root@odoo.(none)>
2015-09-29 17:14:28 -0400

    ok20

--- open_cliente2/models1.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from openerp import models, fields, api
-
-class res_partner(models.Model):
-	_name = "res.partner"
-	_inherit = "res.partner"
-
-	x_cod_cliente = fields.Char(string="cod cliente")
