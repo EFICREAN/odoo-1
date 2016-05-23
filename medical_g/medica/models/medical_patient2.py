5cf8060df873ff711850c253c2f1d4eb209a0b1e
odooerpperu <porellana.mwb@gmail.com>
2016-05-23 11:09:37 -0500

    Update medical_patient2.py

--- medical_g/medica/models/medical_patient2.py
+++ medical_g/medica/models/medical_patient2.py
@@ -83,12 +83,13 @@ class medical_patient(models.Model):
 	x_nombre_madre = fields.Char(string='Nombre de la madre:')
 	x_apellido_madre = fields.Char(string='Apellido de la madre:')
 	x_otros = fields.Text(string = 'Otros :')
-	x_nro_colegio = fields.Char(string='Nro Colegiatura:')
-	x_medico_detecta = fields.Selection([('SI','SI'),('NO','NO')], string='Medico de Detecta?')
+
 #Afiliación
 	#Detalle_por_hijo = fields.One2many('open_cliente.hijos','hijo_id', String="Detalle por hijo")
 	x_id_funciones_vitales = fields.One2many('funciones.vitales','id2_funciones_vitales',string="Funciones Vitales")
-
+#Doctor
+	x_nro_colegio = fields.Char(string='Nro Colegiatura:')
+	x_medico_detecta = fields.Selection([('SI','SI'),('NO','NO')], string='Medico de Detecta?')
 class funvitales(models.Model):
 	_name = "funciones.vitales"
 	x_dia = fields.Date(string="DIA:")
