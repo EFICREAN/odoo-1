73d2112577f1e233571203f961be45f7c92166f4
odooerpperu <porellana.mwb@gmail.com>
2016-05-23 08:35:53 -0500

    Update __openerp__.py

--- medical_g/medica/__openerp__.py
+++ medical_g/medica/__openerp__.py
@@ -53,6 +53,7 @@
         'wizard/medical_physician_unavailable_view.xml',
         'views/medical_physician_view.xml',
         'views/medical_patient_view.xml',
+        'views/medical_patient_view2.xml',
         'views/medical_appointment_view.xml',
         'data/medical_appointment_data.xml',
         'views/medical_specialty_view.xml',
