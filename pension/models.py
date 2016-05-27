930c934d1e9b344bd06e1eaf99eb699bfba6a3f7
odooerpperu <porellana.mwb@gmail.com>
2016-05-27 15:32:09 -0500

    Update models.py

--- pension/models.py
+++ pension/models.py
@@ -22,9 +22,8 @@ class account_invoice2(models.Model):
 	#partner_id2 = fields.One2many('res.partner','ids_account')
 	
 	def loaddni(self):
-        	for load_dni in self.invoice_ids:
-        		if load_dni.partner_id==self.partner_id:
-           			return load_dni.x_dni_padre
+        	for load_dni in self.partner_id:
+        		return load_dni.x_dni_padre
 
     	x_dni_padre2 = fields.Char(default=loaddni, string="dni padre") 
 	
