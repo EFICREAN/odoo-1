dd795f2e8878a6f3215c3400f0ad9615e89640b1
odooerpperu <porellana.mwb@gmail.com>
2016-05-29 13:04:19 -0500

    Update models.py

--- pension/models.py
+++ pension/models.py
@@ -16,13 +16,5 @@ class res_partner(models.Model):
 	x_nivel = fields.Selection([('INICIAL','INICIAL'),('PRIMARIA','PRIMARIA'),('SECUNDARIA','SECUNDARIA')],string="Nivel estudio")
 	#ids_account = fields.Many2one('account.invoice')
 	
-class custom_account(osv.osv):
-    _inherits = 'account.invoice'
-    
-x_dni2 = fields.Char(string="Dni padre")
-@api.multi
-def onchange_partner_id2(self, partner_id):
-	if partner_id:
-            p = self.env['res.partner'].browse(partner_id)
-            self.x_dni2 = p.x_dni_padre
+
             
