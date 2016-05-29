edd2e6632a8f3f0bff546e7e4558243920924364
odooerpperu <porellana.mwb@gmail.com>
2016-05-29 13:17:38 -0500

    Update models2.py

--- pension/models2.py
+++ pension/models2.py
@@ -4,12 +4,12 @@ from openerp import models, fields, api
 from openerp.osv import osv
 
 class custom_account(models.Model):
-    _inherits = 'account.invoice'
+    	_inherits = 'account.invoice'
     
-x_dni2 = fields.Char(string="Dni padre")
+	x_dni2 = fields.Char(string="Dni padre")
 
-@api.multi
-def onchange_partner_id2(self, partner_id):
-	if partner_id:
-            p = self.env['res.partner'].browse(partner_id)
-            self.x_dni2 = p.x_dni_padre
+	@api.multi
+	def onchange_partner_id2(self, partner_id):
+		if partner_id:
+            		p = self.env['res.partner'].browse(partner_id)
+            		self.x_dni2 = p.x_dni_padre
