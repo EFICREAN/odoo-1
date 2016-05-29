d08b6eea4b8d656de07216beb2e7fe368a392620
odooerpperu <porellana.mwb@gmail.com>
2016-05-29 12:56:07 -0500

    Update models.py

--- pension/models.py
+++ pension/models.py
@@ -20,6 +20,7 @@ class custom_account(osv.osv):
     _inherits = 'account.invoice'
     
 x_dni2 = fields.Char(string="Dni padre")
+@api.multi
 def onchange_partner_id2(self, partner_id):
 	if partner_id:
             p = self.env['res.partner'].browse(partner_id)
