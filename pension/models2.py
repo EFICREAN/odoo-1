8e1ccba6e0314e78f52e26e1e3cc1110b93b7f93
odooerpperu <porellana.mwb@gmail.com>
2016-05-29 13:05:13 -0500

    Update models2.py

--- pension/models2.py
+++ pension/models2.py
@@ -3,49 +3,12 @@
 from openerp import models, fields, api
 from openerp.osv import osv
 
-class res_partner(models.Model):
-	_name = "res.partner"
-	_inherit = "res.partner"
-	x_nrodoc = fields.Char(string='DNI del alumno:')
-	x_edad   = fields.Integer(string='Edad:')
-	x_grado1 = fields.Char(string='Grado:')
-	x_seccion1 = fields.Char(string='Sección:')
-	x_nombre_padre = fields.Char(string="Nombre Padre / Tutor")
-	x_dni_padre = fields.Char(string="DNI del Padre")
-	x_movil_padre = fields.Char(string="Movil del Padre")
-	x_nivel = fields.Selection([('INICIAL','INICIAL'),('PRIMARIA','PRIMARIA'),('SECUNDARIA','SECUNDARIA')],string="Nivel estudio")
-	#ids_account = fields.Many2one('account.invoice')
-	
-class custom_account(osv.osv):
+class custom_account(Models.model):
     _inherits = 'account.invoice'
-
-    _columns = {
-       # 'x_dni_padre2': fields.related('partner_id','x_dni_padre',type="char",relation="res.partner",string="dni padre",store=True,readonly=True),
-    	'x_dni_padre3': fields.many2one('res.partner.x_dni_padre', 'Dni del padre2')
-    }
-custom_account()	
-	
-	
-#class account_invoice2(models.Model):
-#	_name = "account.invoice"
-#	_inherit = "account.invoice"
-
-	#partner_id2 = fields.One2many('res.partner','ids_account')
-	
-#	def loaddni(self):
-#		for load_dni in self.partner_id:
- #       	    return load_dni.x_dni_padre
-        	    
-
-  #  	x_dni_padre2 = fields.Char(default=loaddni, string="dni padre") 
-	
-	
-#	x_dni_p = fields.Char(string='Dni padre:')
-	
-	#comment = fields.Many2one('res.partner','Comment',compute='_compute_com')
-	
-	#commercial_partner_id = fields.Many2one('res.partner', string='Commercial Entity',
-        #related='partner_id.commercial_partner_id', store=True, readonly=True,
-        #help="The commercial entity that will be used on Journal Entries for this invoice")
-	
-	
+    
+x_dni2 = fields.Char(string="Dni padre")
+@api.multi
+def onchange_partner_id2(self, partner_id):
+	if partner_id:
+            p = self.env['res.partner'].browse(partner_id)
+            self.x_dni2 = p.x_dni_padre
