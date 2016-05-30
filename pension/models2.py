8bfac74488ce0d97421caa7fe5d0542e55a2f0f7
odooerpperu <porellana.mwb@gmail.com>
2016-05-30 11:31:54 -0500

    Update models2.py

--- pension/models2.py
+++ pension/models2.py
@@ -8,10 +8,10 @@ class custom_account(models.Model):
     	_inherit = "account.invoice"
     
 	x_dni2 = fields.Char(string="Dni padre")
-
-	@api.onchange("partner_id")
-	def _onchange_partner_id2(self):
-		p = self.env['res.partner'].search([('partner_id', '=', partner_id)])
-		for pp in p:
-			self.x_dni2 = pp['x_dni_padre']
-			print('dni padre : %s',self.x_dni2)
+	
+	#@api.onchange("partner_id")
+	#def _onchange_partner_id2(self):
+	#	p = self.env['res.partner'].search([('partner_id', '=', partner_id)])
+	#	for pp in p:
+	#		self.x_dni2 = pp['x_dni_padre']
+	#		print('dni padre : %s',self.x_dni2)
