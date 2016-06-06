280930aad1f97e4c98c270185c12b2a825fec611
odooerpperu <porellana.mwb@gmail.com>
2016-06-06 17:51:05 -0500

    Update models.py

--- pension/models.py
+++ pension/models.py
@@ -5,7 +5,7 @@ from openerp import models, fields, api
 class res_partner2(models.Model):
 	_name = "res.partner"
 	_inherit = "res.partner"
-	nrodoc = fields.Integer(string='DNI del alumno:')
+	nrodoc = fields.Char(string='DNI del alumno:')
 	edad   = fields.Integer(string='Edad:')
 	grado = fields.Char(string='Grado:')
 	seccion = fields.Char(string='Sección:')
@@ -15,5 +15,13 @@ class res_partner2(models.Model):
 	nivel_est = fields.Selection([('INICIAL','INICIAL'),('PRIMARIA','PRIMARIA'),('SECUNDARIA','SECUNDARIA')],string="Nivel estudio")
 	#ids_account = fields.Many2one('account.invoice')
 	
-
+  	@api.onchange('nrodoc')
+    	def _verify_valid_fields(self):
+        	if not self.nrodoc.isdigit() or not len(self.nrodoc)=8:
+            		return {
+                		'warning': {
+                    		'title': "Incorrect DNI value",
+                    		'message': "Es menor a 8 dígitos o no es númerico",
+                		},
+            		}
             
