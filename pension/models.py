67e841b66570fd1c55ffe0e98cba712e74b1f6cb
odooerpperu <porellana.mwb@gmail.com>
2016-06-06 15:45:07 -0500

    Update models.py

--- pension/models.py
+++ pension/models.py
@@ -5,14 +5,14 @@ from openerp import models, fields, api
 class res_partner2(models.Model):
 	_name = "res.partner"
 	_inherit = "res.partner"
-	x_nrodoc = fields.Char(string='DNI del alumno:')
-	x_edad   = fields.Integer(string='Edad:')
-	x_grado1 = fields.Char(string='Grado:')
-	x_seccion1 = fields.Char(string='Sección:')
-	x_nombre_padre = fields.Char(string="Nombre Padre / Tutor")
-	x_dni_padre = fields.Char(string="DNI del Padre")
-	x_movil_padre = fields.Char(string="Movil del Padre")
-	x_nivel = fields.Selection([('INICIAL','INICIAL'),('PRIMARIA','PRIMARIA'),('SECUNDARIA','SECUNDARIA')],string="Nivel estudio")
+	nrodoc = fields.Integer(string='DNI del alumno:')
+	edad   = fields.Integer(string='Edad:')
+	grado = fields.Char(string='Grado:')
+	seccion = fields.Char(string='Sección:')
+	nombre_padre = fields.Char(string="Nombre Padre / Tutor")
+	dni_padre = fields.Integer(string="DNI del Padre")
+	movil_padre = fields.Char(string="Movil del Padre")
+	nivel_est = fields.Selection([('INICIAL','INICIAL'),('PRIMARIA','PRIMARIA'),('SECUNDARIA','SECUNDARIA')],string="Nivel estudio")
 	#ids_account = fields.Many2one('account.invoice')
 	
 
