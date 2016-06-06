342f803eb81d23df680183042991381edcee4723
odooerpperu <porellana.mwb@gmail.com>
2016-06-06 15:59:45 -0500

    Update models2.py

--- pension/models2.py
+++ pension/models2.py
@@ -7,18 +7,18 @@ class custom_account(models.Model):
     	_name = "account.invoice"
     	_inherit = "account.invoice"
     
-	x_grado1 = fields.Char(string='Grado:')
-	x_seccion1 = fields.Char(string='Sección:')
-	x_nivel = fields.Selection([('INICIAL','INICIAL'),('PRIMARIA','PRIMARIA'),('SECUNDARIA','SECUNDARIA')],string="Nivel estudio")
+	grado = fields.Char(string='Grado:',related="partner_id.grado")
+	seccion = fields.Char(string='Sección:',related="partner_id.seccion")
+	nivel_est = fields.Selection([('INICIAL','INICIAL'),('PRIMARIA','PRIMARIA'),('SECUNDARIA','SECUNDARIA')],string="Nivel estudio",related="partner_id.nivel_est")
 
 class custom_account_v(models.Model):
     	_name = "account.voucher"
     	_inherit = "account.voucher"
     
-	x_grado1 = fields.Char(string='Grado:')
-	x_seccion1 = fields.Char(string='Sección:')
-	x_nivel = fields.Selection([('INICIAL','INICIAL'),('PRIMARIA','PRIMARIA'),('SECUNDARIA','SECUNDARIA')],string="Nivel estudio")
-	
+	grado = fields.Char(string='Grado:',related="partner_id.grado")
+	seccion = fields.Char(string='Sección:',related="partner_id.seccion")
+	nivel_est = fields.Selection([('INICIAL','INICIAL'),('PRIMARIA','PRIMARIA'),('SECUNDARIA','SECUNDARIA')],string="Nivel estudio",related="partner_id.nivel_est")
+
 	#@api.onchange("partner_id")
 	#def _onchange_partner_id2(self):
 	#	p = self.env['res.partner'].search([('partner_id', '=', partner_id)])
