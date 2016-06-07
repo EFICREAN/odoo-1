8950d6d217008d91064c0dae49bb8a370dac6835
odooerpperu <porellana.mwb@gmail.com>
2016-06-06 21:23:55 -0500

    Update models.py

--- pension/models.py
+++ pension/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from openerp import models, fields, api
+from openerp.exceptions import ValidationError
 
 class res_partner2(models.Model):
 	_name = "res.partner"
@@ -15,31 +16,20 @@ class res_partner2(models.Model):
 	nivel_est = fields.Selection([('INICIAL','INICIAL'),('PRIMARIA','PRIMARIA'),('SECUNDARIA','SECUNDARIA')],string="Nivel estudio")
 	#ids_account = fields.Many2one('account.invoice')
 	
-  	@api.onchange('nrodoc')
-    	def _verify_valid_nrodoc(self):
+	@api.one
+        @api.constrains('nrodoc')
+    	def _check_nrodoc(self):
     		var_nrodoc = self.nrodoc
-    		if len(var_nrodoc)=0:
+    		if len(var_nrodoc)==0:
     	  		var_nrodoc="1"
-    	  	
     		if not var_nrodoc.isdigit() or not (len(var_nrodoc)==8):
-            		return {
-                		'warning': {
-                    		'title': "Incorrect DNI value",
-                    		'message': "Es menor a 8 dígitos o no es númerico",
-                		},
-            		}
+            		raise ValidationError("Error ingreso DNI alumno")
             		
-        @api.onchange('dni_padre')
-    	def _verify_valid_dnipadre(self):
+        @api.one
+        @api.constrains('dni_padre')
+    	def _check_dni_padre(self):
     	  	var_dni_padre = self.dni_padre
-    	  	if len(var_dni_padre)=0:
+    	  	if len(var_dni_padre)==0:
     	  		var_dni_padre="1"
-    	  	
     		if not var_dni_padre.isdigit() or not (len(var_dni_padre)==8):
-            		return {
-                		'warning': {
-                    		'title': "Incorrect DNI value",
-                    		'message': "Es menor a 8 dígitos o no es númerico",
-                		},
-            		}
-            	
+            		raise ValidationError("Error ingreso DNI padre")	
