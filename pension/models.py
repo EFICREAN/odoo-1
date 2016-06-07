7c3f57f67705ce4ea506e89e840f6ebda5791f63
odooerpperu <porellana.mwb@gmail.com>
2016-06-06 21:50:50 -0500

    Update models.py

--- pension/models.py
+++ pension/models.py
@@ -6,14 +6,14 @@ from openerp.exceptions import ValidationError
 class res_partner2(models.Model):
 	_name = "res.partner"
 	_inherit = "res.partner"
-	nrodoc = fields.Char(string='DNI del alumno:')
-	edad   = fields.Integer(string='Edad:')
-	grado = fields.Char(string='Grado:')
-	seccion = fields.Char(string='Sección:')
-	nombre_padre = fields.Char(string="Nombre Padre / Tutor")
-	dni_padre = fields.Char(string="DNI del Padre")
+	nrodoc = fields.Char(string='DNI del alumno:', required=True)
+	edad   = fields.Integer(string='Edad:', required=True)
+	grado = fields.Char(string='Grado:',required=True)
+	seccion = fields.Char(string='Sección:',required=True)
+	nombre_padre = fields.Char(string="Nombre Padre / Tutor",required=True)
+	dni_padre = fields.Char(string="DNI del Padre",required=True)
 	movil_padre = fields.Char(string="Movil del Padre")
-	nivel_est = fields.Selection([('INICIAL','INICIAL'),('PRIMARIA','PRIMARIA'),('SECUNDARIA','SECUNDARIA')],string="Nivel estudio")
+	nivel_est = fields.Selection([('INICIAL','INICIAL'),('PRIMARIA','PRIMARIA'),('SECUNDARIA','SECUNDARIA')],required=True,string="Nivel estudio")
 	#ids_account = fields.Many2one('account.invoice')
 	
 	@api.one
