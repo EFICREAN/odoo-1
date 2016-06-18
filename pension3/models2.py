b0087ad64b72a5bc59e70451b1304b985721d293
odooerpperu <porellana.mwb@gmail.com>
2016-06-18 17:21:33 -0500

    Update models2.py

--- pension3/models2.py
+++ pension3/models2.py
@@ -8,11 +8,18 @@ class res_partner1(models.Model):
     	_name = "res.partner"
     	_inherit = "res.partner"
     
-    	@api.one
-	@api.constrains('nrodoc')
-	def _check_unique_constraint(self):
-        	if len(self.search(['nrodoc', '=', self.nrodoc])) > 1:
-                	raise ValidationError("DNI del Alumno existe debe ser único")
+    	 _sql_constraints = [
+        ('nrodoc_unique',
+         'UNIQUE(nrodoc)',
+         "DNI del alumno debe ser único")
+    	]
+    	
+    	#@api.one
+	#@api.constrains('nrodoc')
+	#def _check_unique_constraint(self):
+	#	s_count = self.search_count(['nrodoc', '=', self.nrodoc])
+        #	if len(self.search(['nrodoc', '=', self.nrodoc])) > 1:
+         #       	raise ValidationError("DNI del Alumno existe debe ser único")
                 	
                 	
 	
