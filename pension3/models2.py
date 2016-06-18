808dfd8a40e8262ecfbda6499d3474c2cc535fcc
odooerpperu <porellana.mwb@gmail.com>
2016-06-18 16:59:29 -0500

    Update models2.py

--- pension3/models2.py
+++ pension3/models2.py
@@ -9,11 +9,10 @@ class res_partner1(models.Model):
     	_inherit = "res.partner"
     
     	@api.one
-	@api.constrains('nrodoc','dni_padre')
+	@api.constrains('nrodoc')
 	def _check_unique_constraint(self):
         	if len(self.search(['nrodoc', '=', self.nrodoc])) > 1:
                 	raise ValidationError("DNI del Alumno existe debe ser único")
-                if len(self.search(['dni_padre', '=', self.nrodoc])) > 1:
-                	raise ValidationError("DNI del padre existe debe ser único")
+                	
                 	
 	
