24413f04cbe3a31507d28945979e0fa66a80a9a7
odooerpperu <porellana.mwb@gmail.com>
2016-06-18 16:28:59 -0500

    Update models2.py

--- pension3/models2.py
+++ pension3/models2.py
@@ -1,27 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from openerp import models, fields, api
+from openerp.exceptions import ValidationError
 
 
-class custom_account(models.Model):
-    	_name = "account.invoice"
-    	_inherit = "account.invoice"
+class res_partner1(models.Model):
+    	_name = "res.partner"
+    	_inherit = "res.partner"
     
-	grado = fields.Char(string='Grado:',related="partner_id.grado")
-	seccion = fields.Char(string='Sección:',related="partner_id.seccion")
-	nivel_est = fields.Selection([('INICIAL','INICIAL'),('PRIMARIA','PRIMARIA'),('SECUNDARIA','SECUNDARIA')],string="Nivel estudio",related="partner_id.nivel_est")
-
-class custom_account_v(models.Model):
-    	_name = "account.voucher"
-    	_inherit = "account.voucher"
-    
-	grado = fields.Char(string='Grado:',related="partner_id.grado")
-	seccion = fields.Char(string='Sección:',related="partner_id.seccion")
-	nivel_est = fields.Selection([('INICIAL','INICIAL'),('PRIMARIA','PRIMARIA'),('SECUNDARIA','SECUNDARIA')],string="Nivel estudio",related="partner_id.nivel_est")
-
-	#@api.onchange("partner_id")
-	#def _onchange_partner_id2(self):
-	#	p = self.env['res.partner'].search([('partner_id', '=', partner_id)])
-	#	for pp in p:
-	#		self.x_dni2 = pp['x_dni_padre']
-	#		print('dni padre : %s',self.x_dni2)
+    	@api.one
+	@api.constraints('nrodoc','dni_padre')
+	def _check_unique_constraint(self):
+        	if len(self.search(['nrodoc', '=', self.nrodoc])) > 1:
+                	raise ValidationError("DNI del Alumno existe debe ser único")
+                if len(self.search(['dni_padre', '=', self.nrodoc])) > 1:
+                	raise ValidationError("DNI del padre existe debe ser único")
+                	
+	
