f710ba04249a78535c541895471438ef3df2a4c4
odooerpperu <porellana.mwb@gmail.com>
2016-06-19 22:30:08 -0500

    Update models.py

--- pension/models.py
+++ pension/models.py
@@ -6,14 +6,14 @@ from openerp.exceptions import ValidationError
 class res_partner2(models.Model):
 	_name = "res.partner"
 	_inherit = "res.partner"
-	nrodoc = fields.Char(string='DNI del alumno:', required=True)
-	edad   = fields.Integer(string='Edad:', required=True)
-	grado = fields.Char(string='Grado:',required=True)
-	seccion = fields.Char(string='Sección:',required=True)
-	nombre_padre = fields.Char(string="Nombre Padre / Tutor",required=True)
-	dni_padre = fields.Char(string="DNI del Padre",required=True)
+	nrodoc = fields.Char(string='DNI del alumno:', required=False)
+	edad   = fields.Integer(string='Edad:', required=False)
+	grado = fields.Char(string='Grado:',required=False)
+	seccion = fields.Char(string='Sección:',required=False)
+	nombre_padre = fields.Char(string="Nombre Padre / Tutor",required=False)
+	dni_padre = fields.Char(string="DNI del Padre",required=False)
 	movil_padre = fields.Char(string="Movil del Padre")
-	nivel_est = fields.Selection([('INICIAL','INICIAL'),('PRIMARIA','PRIMARIA'),('SECUNDARIA','SECUNDARIA')],required=True,string="Nivel estudio")
+	nivel_est = fields.Selection([('INICIAL','INICIAL'),('PRIMARIA','PRIMARIA'),('SECUNDARIA','SECUNDARIA')],required=False,string="Nivel estudio")
 	mobile = fields.Char(string="movil",related="movil_padre")
 	#ids_account = fields.Many2one('account.invoice')
 	
