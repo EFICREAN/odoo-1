429c5a1436b2dc1023683bdd5aa068b13e86cbf9
odooerpperu <porellana.mwb@gmail.com>
2016-06-19 22:28:28 -0500

    Update models2.py

--- pension3/models2.py
+++ pension3/models2.py
@@ -9,6 +9,31 @@ class res_partner1(models.Model):
     	_inherit = "res.partner"
     	
     	@api.one
+        @api.constrains('nrodoc')
+    	def _check_nrodoc(self):
+    		var_nrodoc = self.nrodoc
+    		if len(var_nrodoc)==0:
+    	  		var_nrodoc="1"
+    		if not var_nrodoc.isdigit() or not (len(var_nrodoc)==8):
+            		raise ValidationError("Error ingreso DNI alumno")
+            		
+        @api.one
+        @api.constrains('dni_padre')
+    	def _check_dni_padre(self):
+    	  	var_dni_padre = self.dni_padre
+    	  	if len(var_dni_padre)==0:
+    	  		var_dni_padre="1"
+    		if not var_dni_padre.isdigit() or not (len(var_dni_padre)==8):
+            		raise ValidationError("Error ingreso DNI padre")	
+
+	@api.one
+        @api.constrains('edad')
+    	def _check_edad(self):
+    		if self.edad==0:
+            		raise ValidationError("Error Edad")
+
+    	
+    	@api.one
 	@api.constrains('nrodoc', 'dni_padre')
 	def _check_dni(self):
     		if self.nrodoc == self.dni_padre:
