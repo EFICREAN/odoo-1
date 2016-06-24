272f748aec48f0138878786d9390acad7aa03a38
odooerpperu <porellana.mwb@gmail.com>
2016-06-24 13:37:00 -0500

    Update __openerp__.py

--- odoosms2/__openerp__.py
+++ odoosms2/__openerp__.py
@@ -12,7 +12,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Tools',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -20,7 +20,8 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        "security/groups.xml",
+        "security/ir.model.access.csv"
     ],
     # only loaded in demonstration mode
     'demo': [
