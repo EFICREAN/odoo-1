6cf483602f09b77cce59eaf2a1d307830a2ac739
Paul <porellana.mwb@gmail.com>
2017-09-08 22:03:26 +0000

    tpv_2

---
+++ tpv_2/__openerp__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Personalizacióel Punto de Venta',
+    'version': '1.0',
+    'website' : '',
+    'category': '',
+    'summary': '',
+    'description': """
+Personalizacióel Terminal de Punto de Venta
+======================================================
+Este móo añ Personalizaciones al móo 
+Terminal de Punto de Venta. 
+""",
+    'author': 'Juan Salcedo',
+    'depends': [
+        'point_of_sale',
+    ],
+    'data': [
+        'point_of_sale_view.xml',
+        'views/templates.xml',
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'qweb': [
+    ],
+}
+
